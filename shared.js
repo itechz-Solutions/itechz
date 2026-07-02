7ea9d023d094b487f2ed7a1da05fb97ee1b931fe
iTechzsolutions <itechz1solutions@gmail.com>
2026-07-02 23:14:49 +0530

    Update shared.js

--- shared.js
+++ shared.js
@@ -25,11 +25,11 @@ const NAV_HTML = `
 
 const FOOTER_HTML = `
 <div class="contact-bar">
-  <a class="contact-bar-item" href="tel:+918605400518">
+  <a class="contact-bar-item" href="tel:+918698529411">
     <svg viewBox="0 0 24 24"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.6 1.27h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.84a16 16 0 0 0 6.29 6.29l.92-.92a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z"/></svg>
     +91 86054 00518
   </a>
-  <a class="contact-bar-item" href="https://wa.me/918605400518" target="_blank">
+  <a class="contact-bar-item" href="https://wa.me/918698529411" target="_blank">
     <svg viewBox="0 0 24 24"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"/></svg>
     WhatsApp: 86054 00518
   </a>
